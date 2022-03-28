e79009de6268c3bab2a0731d7af1f9af56eb63cf
Alangobe <alangobe01@gmail.com>
2022-03-27 22:12:08 -0500

    Añadida vista y urls de api

--- AppDjango/api/views.py
+++ AppDjango/api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import generics
+from .models import Posts
+from .serializers import PostSerializer
 
 # Create your views here.
+
+class PostView(generics.RetrieveAPIView):
+
+    queryset = Posts.objects.all()
+
+    def get(self, request, *args, **kwargs):
+
+        queryset = self.get_queryset()
+        serializer = PostSerializer(queryset, many=True)
+        return Response(serializer.data)
