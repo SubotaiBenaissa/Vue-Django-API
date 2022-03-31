8f67d1e5810522b43f05abe588b33a376263dff9
Alangobe <alangobe01@gmail.com>
2022-03-30 22:17:14 -0500

    Terminados estilos

--- AppDjango/vueapp/vuerest/src/views/PostsView.vue
+++ AppDjango/vueapp/vuerest/src/views/PostsView.vue
@@ -11,11 +11,7 @@
                       <h4 class=""><a class="text-secondary" href="">{{posts.title}}</a></h4>
                       <p class="card-text">{{posts.content}}</p>
                       <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                      <a href="" class="btn btn-sm btn-outline-primary" role="button" aria-pressed="true">View</a>
-                      <a href="" class="btn btn-sm btn-outline-secondary" role="button" aria-pressed="true">Edit</a>
-                      </div>
-                      <small class="text-muted">9 mins</small>
+                      <small class="text-muted">:D</small>
                     </div>
                   </div>
                 </div>
